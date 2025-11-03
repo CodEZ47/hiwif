a9a130b28cdbd60b5d7a03dee1fe920c6647814b
Aman <amanuelabebe2000@gmail.com>
2025-11-03 03:57:14 +0200

    (backend):changed model for toxicity check

--- backend/ai_service/app/moderation_model.py
+++ backend/ai_service/app/moderation_model.py
@@ -2,15 +2,18 @@ from transformers import pipeline
 
 _offense_model = pipeline(
     "text-classification",
-    model="unitary/toxic-bert",
+    model="martin-ha/toxic-comment-model",
+    return_all_scores=True,
     truncation=True
 )
 
 def get_offense_score(text: str) -> float:
     try:
-        result = _offense_model(text[:512])[0]
-        score = float(result["score"])
+        preds = _offense_model(text)[0]
+        offensive_labels = [p["score"] for p in preds if p["label"] not in ["non-toxic", "neutral"]]
+        score = sum(offensive_labels) / len(offensive_labels)
         return round(score, 4)
     except Exception as e:
         print("Moderation model error:", e)
         return 0.0
+
